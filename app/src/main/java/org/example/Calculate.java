941c6a1de40289d10a7c8e724e0afad4e5c31a38
e1c23047 <e1c23047@oit.ac.jp>
2026-07-06 10:27:06 +0900

    calculate test

--- app/src/main/java/org/example/Calculate.java
+++ app/src/main/java/org/example/Calculate.java
@@ -4,4 +4,20 @@ public class Calculate {
     public int sum(int x, int y) {
         return x + y;
     }
+
+    public int getIndividualFee(int age) {
+        if (age < 13) {
+            return 1000; // Fee for children
+        } else {
+            return 1300; // Fee for seniors
+        }
+    }
+    public int getTotalFee(int age , int groupSize) {
+        int individualFee = getIndividualFee(age);
+        int result = individualFee * groupSize;
+        if (groupSize >= 10) {
+            result = (int)(result * 0.9);
+        }
+        return result;
+    }
 }
--- app/src/test/java/org/example/CalculateTest.java
+++ app/src/test/java/org/example/CalculateTest.java
@@ -10,4 +10,10 @@ public class CalculateTest {
         int expected = 5;
         assertEquals(expected, calculate.sum(2, 3), "Sum of 2 and 3 should be 5");
     }
+    @Test
+    public void getTotalFeeTest() {
+        Calculate calculate = new Calculate();
+        int expected = 1000;
+        assertEquals(expected, calculate.getTotalFee(10, 1), "Total fee for age 10 and group size 1 should be 1000");
+    }
 }
